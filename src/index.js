247e593b350f623d26ff8fd972a3bc0c183853a2
bahasaracoglu <bahasaracoglu@gmail.com>
2023-03-21 18:55:22 +0300

    İlk kurulum yapıldı.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-import './less/index.less'
+import "./less/index.less";
 
 // Örnek bir event kullanımı aşağıdadır. Çalıştırmak için comment dışına alın
 // document.querySelector("h1").addEventListener("click",function(e){
 // alert("Bana tıkladın!")
 // });
-
-
 // Kodlar buraya gelecek!
+
+const allP = document.querySelectorAll("p");
+
+allP.addEventListener("mouseover", (event) => {
+  for (let i = 0; i < allP.length; i++) {
+    allP[i].style.backgroundColor = "red";
+  }
+});
+
+const destination = document.getElementsByClassName("destination");
+
+destination.addEventListener("mouseover", (event) => {
+  destination.style.backgroundColor = "yellow";
+});
